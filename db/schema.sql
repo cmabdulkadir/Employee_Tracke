8947633c9b570ed5e3ebc5ddbbda6e4f2eac60b9
Chaltu Abdulkadir <chaltumabdulkadir@gmail.com>
2021-03-18 07:51:54 -0500

    updated file structure and date

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,28 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP DATABASE IF EXISTS employeesDB;
+CREATE DATABASE employeesDB;
 
-USE employees;
+USE employeesDB;
 
 CREATE TABLE department (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
 );
 
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT
 );
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
--- db/seed.sql
+++ db/seed.sql
@@ -1,33 +1,48 @@
-use employees;
+use employeesDB;
 
-INSERT INTO department
-    (name)
-VALUES
-    ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
 
-INSERT INTO role
-    (title, salary, department_id)
-VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
 
-INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
-VALUES
-    ('Chaltu', 'Abdulkadir', 1, NULL),
-    ('Sarah', 'Haye', 2, 1),
-    ('John', 'Reed', 3, NULL),
-    ('Justin', 'Baker', 4, 3),
-    ('Tom', 'Cruise', 5, NULL),
-    ('Nick', 'Stone', 6, 5),
-    ('Matt', 'Comb', 7, NULL),
-    ('Reed', 'Smith', 8, 7);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 91000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 127000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 86000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 115000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 73000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 129000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 210000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Chaltu", "Abdulkadir", 1, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Martin", "Haye", 3, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Nick", "Stone", 1, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Susan", "Smith", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Justin", "Baker", 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tom", "Cruise", 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Carson", "schmit", 2);
